4a65cc68a31a9748d91fc53214c1ee735c3fd3f3
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-12-16 16:40:48 +0100

    Convert uploaded picture in gray colors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ const fileInput = document.querySelector('input[type="file"');
 
 const context = canvas.getContext('2d');
 
+const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
+
+const convertToGrayScales = (context, width, height) => {
+    const imageData = context.getImageData(0, 0, width, height);
+
+    const grayScales = [];
+
+    for (let i = 0 ; i < imageData.data.length ; i += 4) {
+        const r = imageData.data[i];
+        const g = imageData.data[i + 1];
+        const b = imageData.data[i + 2];
+
+        const grayScale = toGrayScale(r, g, b);
+        imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+
+        grayScales.push(grayScale);
+    }
+
+    context.putImageData(imageData, 0, 0);
+
+    return grayScales;
+};
+
 fileInput.onchange = (e) => {
     const file = e.target.files[0];
 
@@ -12,7 +35,9 @@ fileInput.onchange = (e) => {
         image.onload = () => {
             canvas.width = image.width;
             canvas.height = image.height;
+
             context.drawImage(image, 0, 0);
+            convertToGrayScales(context, canvas.width, canvas.height);
         }
 
         image.src = event.target.result;
